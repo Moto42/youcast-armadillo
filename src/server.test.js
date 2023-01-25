139f420a5edaff4cd9c0d21be6786121671533b2
Wesley Williams <moto42@gmail.com>
2023-01-25 17:17:36 -0600

    Now testing in jest

--- src/server.test.js
+++ src/server.test.js
@@ -1,11 +1,11 @@
-const {expect} = require('chai');
-const supertest = require('supertest');``
+const supertest = require('supertest');
 const {app} = require("./server");
+const youcast = require('./youcast');
 
 describe('Server basic tests',()=>{
     it("passes requests to my middleware", async ()=>{
         const request = supertest(app);
-        const response = await request.get("/")
-        expect(response.status).to.eql(200);
+        const response = await request.get("/");
+        expect(response.status).toBe(200);
     });
 });
