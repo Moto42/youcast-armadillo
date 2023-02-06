a35c7a1f8a9299008ec5004a3c1a7a68fff6fe05
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-02-06 15:25:11 -0600

    remove unused import

--- src/youcast/entrypoint.js
+++ src/youcast/entrypoint.js
@@ -1,11 +1,10 @@
 /**
- * Entrypoint is responsibler for accepting things and emitting the inital event to the format
+ * Entrypoint is responsible for accepting things and emitting the inital event to the format
  * it is also where the various plugins (formats and sources) register, so an error can be returned 
  * if a requested plugin does not exist
  */
 
 const {EventEmitter} = require('node:events');
-const { format } = require('node:path');
 const FormatRegistry = require('./FormatsRegistry');
 
 /**
