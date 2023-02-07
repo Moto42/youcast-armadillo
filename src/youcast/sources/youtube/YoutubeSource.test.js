b10ef6e536894d75e7ef5d8d035282e16428b5de
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-02-06 18:08:43 -0600

    Mocked the file system, yay!

--- src/youcast/sources/youtube/YoutubeSource.test.js
+++ src/youcast/sources/youtube/YoutubeSource.test.js
@@ -1,3 +1,26 @@
-it('pass', () => {
-    expect(true).toBeTruthy();
+const YoutubeSource = require('./YoutubeSource');
+const EventEmitter = require('node:events');
+const mockfs = require('mock-fs');
+
+
+describe('can download a video', () => {
+    
+    let source, messageChannel;
+
+    beforeEach(() => {
+        messageChannel = new EventEmitter();
+        source = new YoutubeSource(messageChannel);
+        mockfs();
+    });
+    afterEach(() => {
+        mockfs.restore();
+    });
+    afterAll(() => {
+        mockfs.restore();
+    });
+
+    it('can download a video', () => {
+
+    });
+
 });
