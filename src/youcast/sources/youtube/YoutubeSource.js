51eac4e44de47c35c252932043ae3516d04b30f4
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-02-06 18:08:50 -0600

    yay again!

--- src/youcast/sources/youtube/YoutubeSource.js
+++ src/youcast/sources/youtube/YoutubeSource.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
+const EventEmitter  = require('node:events');
+
+/**
+ * 
+ * @param {EventEmitter} messageChannel 
+ */
 const YoutubeSource = function(messageChannel) {
     this.messageChannel = messageChannel;
     this.name = 'YouTube';
     this.shortcode = 'youtube';
+
+
+    this.downloadVideo = (id) => {
+        fs.writeFileSync("foo.txt", "bar");
+    }
 }
 
 module.exports = YoutubeSource;
