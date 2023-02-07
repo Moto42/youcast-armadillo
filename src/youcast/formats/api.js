593a09c28cd0ab2a7a7440881222c6a9d548adf7
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-02-07 15:36:17 -0600

    docs

--- src/youcast/formats/api.js
+++ src/youcast/formats/api.js
@@ -1,9 +1,20 @@
-// api is a special format for gettings information about the app.
-
+/**
+ * api is a special format for gettings information about the app.
+ * 
+ * @param {*} messageChannel 
+ * @listens api
+ */
 module.exports = function Api(messageChannel) {
     this.name = 'api';
     this.shortcode = 'api';
 
+    /**
+     * build a list of the registered formats and send it to the client
+     * @param {Resquest} req 
+     * @param {Response} res 
+     * @fires listFormats
+     * @listens formatsList
+     */
     this.api_formats = (req, res) => {
         function respondWithList(formats){
             res.status(200);
