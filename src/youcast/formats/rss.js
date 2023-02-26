8a2e8e7bd90b05db961ff976236e79d194b0fb53
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2023-02-26 22:38:04 +0000

    makefile rss.js

---
+++ src/youcast/formats/rss.js
@@ -0,0 +1,17 @@
+/**
+ * handler for when a raw MP3 is requested
+ * @param {EventEmitter}} messageChannel
+ * @returns {Mp3Format}
+ * @property {string} shortcode - the handle for referencing this api in the registry and events
+ * @property {string} name - the human friendly name of this format
+ * @listens rss
+ */
+function RSSFormat(messageChannel) {
+    this.shortcode = 'rss';
+    this.name = 'rss';
+
+}
+
+
+
+module.exports = Mp3Format;
